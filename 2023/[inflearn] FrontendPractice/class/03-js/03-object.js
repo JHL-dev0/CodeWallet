338223f2f49e2eb52da07de866b258b3e03a26fd
JiHyunLim <abcdintj@gmail.com>
2023-11-04 15:07:50 +0900

    오늘의 공부

---
+++ 2023/[inflearn] FrontendPractice/class/03-js/03-object.js
@@ -0,0 +1,22 @@
+let friend = { 
+  name : "철수", 
+  age : 13, 
+  camp : "코드캠프"
+}
+// undefined
+
+friend
+// {name: '철수', age: 13, camp: '코드캠프'}
+
+friend.name
+// '철수'
+
+friend.age
+// 13
+
+friend.camp
+// '코드캠프'
+
+frined.test
+// VM391:1 Uncaught ReferenceError: frined is not defined
+//   at <anonymous>g:1:1
