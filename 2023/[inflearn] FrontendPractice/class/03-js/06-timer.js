f483fce83cf07419b183799237fa9d3bb455a5d5
JHL-dev0 <abcdintj@gmail.com>
2023-11-08 23:20:41 +0900

    11/08

--- 2023/[inflearn] FrontendPractice/class/03-js/06-timer.js
+++ 2023/[inflearn] FrontendPractice/class/03-js/06-timer.js
@@ -1,20 +1,38 @@
-function send1() {
-    const token = String ( Math.floor ( Math.random() * 1000000 ) ).padStart(6, "0");
-    document.getElementById('target').innerText = token
+let isStarted = false;
+
+
+let auth = () => {
+
+    if(isStarted === false){
+        // 타이머가 작동중이 아닐때
+        isStarted = true;
+        document.getElementById('button').disabled = false;
+        const token = String ( Math.floor ( Math.random() * 1000000 ) ).padStart(6, "0");
+        document.getElementById('target').innerText = token
+        
+        // let time = 180;
+        let time = 5; // 5초로 테스트
+        let timer;
+
+        timer = setInterval(function(){
+            if(time >= 0){
+                let min = Math.floor( time / 60 );
+                let sec = String(time % 60).padStart(2, "0")
+                document.getElementById('timer').innerText = (min + ":" + sec)
+                time = time - 1;
+            } else {
+                document.getElementById('timer').innerText = "시간초과"
+                document.getElementById('button').disabled = true;
+                isStarted = false;
+                console.log("타이머 종료")
+                clearInterval(timer) // setInterval을 종료시키는 함수
+            }
+        }, 1000)
     
-    // let time = 180;
-    let time = 5; // 5초로 테스트
-    setInterval(function(){
-        if(time == 0){
-            document.getElementById('timer').innerText = "시간초과"
-            document.getElementById('button').disabled = true;
-        } else {
-            let min = Math.floor( time / 60 );
-            let sec = String(time % 60).padStart(2, "0")
-            document.getElementById('timer').innerText = (min + ":" + sec)
-            time = time - 1;
-        }
-    }, 1000)
+    }else{
+        // 타이머가 작동중일때
+    }
+
 
 }
 
